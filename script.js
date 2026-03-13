c809541bd2c6c57791ed6a207f610adf08c30192
GANESH-KORGAONKAR <ganeshkorgaonkar4335@gmail.com>
2026-03-13 16:36:23 +0530

    feat: add matrix-style random text animation and rename project
    - implemented randomText() function for scrambling characters
    - text gradually resolves to original string
    - feature still under development

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
+const H1 = document.querySelector(".H1");
+const para = document.querySelector(".para");
+const btn = document.querySelector(".btn");
+
+const h1Text = H1.innerText;
+const paraText = para.innerText;
+const btnText = btn.innerText;  
+
+const charachters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; //62 characters.
+
+let iteration = 0;
+
+function randomText(){
+    const str = h1Text.split("").map((char, index) => {
+        if (index < iteration) {
+            return char
+        }
+        return charachters.split("")[Math.floor(Math.random() * charachters.length)]
+    }).join("");
+    
+    H1.innerText = str;
+
+    iteration += 0.25
+}
+
+setInterval ( randomText, 30);
+
 addEventListener("mousemove", (e) => {
-    console.log(e.clientX, e.clientY);
+    console.log(e.clientX, e.clientY); 
     document.body.style.setProperty("--x", e.clientX + "px");
     document.body.style.setProperty("--y", e.clientY + "px");
 });
